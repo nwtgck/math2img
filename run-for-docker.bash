c8c79548210cf69e4cbbc959dbff5c473154f01f
Ryo Ota <nwtgck@gmail.com>
2017-11-13 01:30:15 +0900

    [fix] Fix not to be all black when out_ext is jpg

--- run-for-docker.bash
+++ run-for-docker.bash
@@ -18,6 +18,9 @@ if   [ $out_ext = "pdf" ]; then
 elif [ $out_ext = "svg" ]; then
     # Convert .pdf to .svg
     pdf2svg hoge-crop.pdf hoge.svg
+elif [ $out_ext = "jpg" ]; then
+    # Use convert command for jpg with "-alpha Remove" (from: http://risibletons.net/?Blog%2F2013-05-22)
+    convert -density 600 hoge-crop.pdf -quality 100 -resize 300% -alpha Remove hoge.$out_ext
 else
     # Use convert command to convert
     convert -density 600 hoge-crop.pdf -quality 100 -resize 300% hoge.$out_ext
